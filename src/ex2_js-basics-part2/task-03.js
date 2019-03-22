4c747760659f7086475752a84786fdbed2196742
Vladimir <vvtolstykh@gmail.com>
2019-03-22 21:36:12 +0300

    Fix invalid names of variables

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,19 +1,19 @@
 function evenOddZero(myArr){
-  var a = 0;
-  var b = 0;
-  var c = 0;
+  var even = 0;
+  var odd = 0;
+  var zero = 0;
   for (i = 0; i < myArr.length; i++){
     if (typeof(myArr[i]) === 'number' && !isNaN(myArr[i])) {
       if ((myArr[i] % 2) === 0 && myArr[i] !== 0) {
-        a++
+        even++
       } else if (myArr[i] === 0) {
-        c++
+        zero++
       } else {
-        b++
+        odd++
       }
     }
   }
-    return [a, b, c];
+    return [even, odd, zero];
 }
 
 module.exports = evenOddZero;
